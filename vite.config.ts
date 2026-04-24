447a6bcfd9a66337e88b6b697f2ada02066a98c4
iqbalussain <44889346+iqbalussain@users.noreply.github.com>
2026-04-24 21:20:08 +0000

    n

--- vite.config.ts
+++ vite.config.ts
@@ -1,34 +1,27 @@
-import { defineConfig, splitVendorChunkPlugin } from "vite";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig({
   base: "./",
-  logLevel: "warn",
 
-  plugins: [react(), splitVendorChunkPlugin()],
-
-  server: {
-    host: "::",
-    port: 5173,
-    hmr: {
-      overlay: false,
-    },
-  },
-
-  build: {
-    outDir: "dist",
-    emptyOutDir: true,
-    sourcemap: false,
-    target: "es2020",
-    minify: "esbuild" as const,
-    cssCodeSplit: true,
-    assetsInlineLimit: 4096,
-  },
+  plugins: [react()],
 
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
+
+  server: {
+    host: "::",
+    port: 5173,
+  },
+
+  build: {
+    outDir: "dist",
+    sourcemap: false,
+    minify: "esbuild",
+    emptyOutDir: true
+  }
 });
